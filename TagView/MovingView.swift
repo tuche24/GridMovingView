b1e9c2c8c991593875f100b37cc8d5c5dae39367
tuche24 <tuche24@naver.com>
2023-06-02 14:15:07 +0900

    GridMovingView

--- TagView/MovingView.swift
+++ TagView/MovingView.swift
@@ -1,5 +1,9 @@
 import UIKit
 
+protocol MovingViewDelegate: AnyObject {
+    func tagViewDidMove(_ view: MovingView)
+}
+
 enum MovingViewState {
     case began
     case ended
@@ -16,6 +20,12 @@ class MovingView: UIView {
     private var dragStartLocation: CGPoint = .zero
     private var originalPosition: CGPoint = .zero
     private let dashedBorder = CAShapeLayer()
+    
+    private var snapPoints: [CGPoint] = []
+    private var horizontalSnapCount = 4
+    private var verticalSnapCount = 4
+    
+    var delegate: MovingViewDelegate?
 
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -30,7 +40,21 @@ class MovingView: UIView {
     private func setupView() {
         layer.cornerRadius = 10
         
+        setupSnapPoints()
         setupGesture()
+    }
+    
+    private func setupSnapPoints() {
+        snapPoints.removeAll()
+        let gridSize: CGFloat = 10.0
+        
+        for i in 0..<horizontalSnapCount {
+            for j in 0..<verticalSnapCount {
+                let x = CGFloat(i) * (frame.width + gridSize) + gridSize
+                let y = CGFloat(j) * (frame.height + gridSize) + gridSize
+                snapPoints.append(CGPoint(x: x, y: y))
+            }
+        }
     }
     
     private func setupGesture() {
@@ -47,7 +71,7 @@ class MovingView: UIView {
         case .changed:
             updatePosition(with: gesture)
         case .ended, .cancelled:
-            state = .ended
+            finishDrag()
         default:
             break
         }
@@ -57,6 +81,37 @@ class MovingView: UIView {
         let locationInSuperview = gesture.location(in: superview)
         frame.origin = CGPoint(x: locationInSuperview.x - dragStartLocation.x,
                                 y: locationInSuperview.y - dragStartLocation.y)
+    }
+    
+    private func finishDrag() {
+        delegate?.tagViewDidMove(self)
+        state = .ended
+        
+        if let closestSnapPoint = snapPoints.min(by: { distance($0, frame.origin) < distance($1, frame.origin) }) {
+            UIView.animate(withDuration: 0.2) {
+                self.frame.origin = closestSnapPoint
+            }
+        }
+        
+        DispatchQueue.main.async { [self] in
+            if let superview = superview {
+                for subview in superview.subviews {
+                    if let tagView = subview as? MovingView, tagView != self, isOverlapping(with: tagView) {
+                        UIView.animate(withDuration: 0.2) {
+                            self.frame.origin = self.originalPosition
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    private func distance(_ point1: CGPoint, _ point2: CGPoint) -> CGFloat {
+        return sqrt(pow(point1.x - point2.x, 2) + pow(point1.y - point2.y, 2))
+    }
+    
+    func isOverlapping(with other: MovingView) -> Bool {
+        return frame.intersects(other.frame)
     }
     
     private func handleStateChange() {
